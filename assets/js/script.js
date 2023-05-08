8936959801839224202896ec54e919d3604b8799
Gabriel Regus <gabriel.regus@gmail.com>
2023-05-08 06:22:17 -0400

    adding open weather api js

---
+++ assets/js/script.js
@@ -0,0 +1,5 @@
+var apiKey = "9a416b3b0fea4d6e4f9dca421a3b8b21";
+var city = "";
+var searchHistory = [];
+
+var QueryUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=' + apiKey;
